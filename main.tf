55c9be4b2123ff44a90a05ddbf5081292ce5db18
Marcelo Oliveira <marcelo@marceloweb.info>
2021-11-04 17:43:34 +0000

    fix: terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "example" {
+
+resource "aws_instance" "web" {
   ami = "ami-0c6b1d09930fac512"
   instance_type = "t2.micro"
   count = 2
-  tags {
-    Name = "example"
+
+  tags = {
+    Name = "myWebInstance"
   }
 }
