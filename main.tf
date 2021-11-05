925afeea844366b97cbeec9058889c2ba79ff245
Marcelo Oliveira <marcelo@marceloweb.info>
2021-11-05 21:12:02 +0000

    added key pair

--- main.tf
+++ main.tf
@@ -14,9 +14,14 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami = "ami-0c6b1d09930fac512"
   instance_type = "t2.micro"
-  count = 2
+  count = 1
+  key_name = "tutorial"
 
   tags = {
     Name = "myWebInstance"
   }
+}
+
+output "instance_ips" {
+  value = aws_instance.web.*.public_ip
 }
