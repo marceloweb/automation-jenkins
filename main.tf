e1b798a001f5827e42df0edd8a21d74543c96c01
Marcelo Oliveira <marcelo@marceloweb.info>
2021-11-08 23:05:42 +0000

    ssh/ansible

--- main.tf
+++ main.tf
@@ -35,8 +35,8 @@ resource "aws_instance" "web" {
   provisioner "remote-exec" {
      inline = [
         "sudo apt install software-properties-common",
-        "sudo add-apt-repository --yes --update ppa:ansible/ansible"
-        "sudo apt install -y ansible"
+        "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+        "sudo apt install -y ansible",
         "ansible --version"
      ]
   }
