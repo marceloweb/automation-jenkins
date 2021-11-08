999a87704950c01c4800a88c3a407d103abf5c51
Marcelo Oliveira <marcelo@marceloweb.info>
2021-11-08 22:48:41 +0000

    ssh

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "web" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = "file(tutorial.pem)"
+    private_key = "${file("tutorial.pem")}"
     host        = "${self.public_ip}"
   }
 
